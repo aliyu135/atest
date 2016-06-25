ba4019d74162030213929924aeb6683ff29ba5b1
aliyu135 <a15239235137@163.com>
2016-06-25 09:31:50 +0800

    第一次提交猫眼

---
+++ CatEye/app/src/main/java/atguigu/com/cateye/SplainActivity.java
@@ -0,0 +1,26 @@
+package atguigu.com.cateye;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplainActivity extends Activity {
+private Handler handler=new Handler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splain);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplainActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+
+
+
+    }
+}
